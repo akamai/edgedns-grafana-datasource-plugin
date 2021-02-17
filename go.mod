ce4a69ea140cc73bbc07d38f12eb45debcbd0dd8
Steve Vernick <svernick@akamai.com>
2021-02-17 16:04:16 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/grafana/simple-datasource-backend
+
+go 1.14
+
+require (
+	github.com/akamai/AkamaiOPEN-edgegrid-golang v1.0.0
+	github.com/go-macaron/session v1.0.2 // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/gosimple/slug v1.9.0 // indirect
+	github.com/grafana/grafana v6.1.6+incompatible
+	github.com/grafana/grafana-plugin-sdk-go v0.80.0
+	github.com/inconshreveable/log15 v0.0.0-20201112154412-8562bdadbbac // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/teris-io/shortid v0.0.0-20201117134242-e59966efd125 // indirect
+)
---
+++ src/QueryEditor.tsx
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2021 Akamai Technologies
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import defaults from 'lodash/defaults';
+
+import React, { ChangeEvent, PureComponent } from 'react';
+import { QueryEditorProps, SelectableValue } from '@grafana/data';
+import { InlineField, LegacyForms, AsyncSelect } from '@grafana/ui';
+import { DataSource } from './DataSource';
+import { defaultQuery, MyDataSourceOptions, MyQuery } from './types';
+
+const { FormField } = LegacyForms;
+
+type Props = QueryEditorProps<DataSource, MyQuery, MyDataSourceOptions>;
+
+export class QueryEditor extends PureComponent<Props> {
+  getReports = () => {
+    const uri = 'datasource/resource/openapireports';
+    return this.props.datasource.getResource(uri);
+  };
+
+  onSelectReportsChange = (item: SelectableValue<string>) => {
+    const { onChange, query, onRunQuery } = this.props;
+    onChange({ ...query, selectedReport: item });
+    console.log('selectedReport: ', item);
+    // executes the query
+    onRunQuery();
+  };
+
+  onZoneNamesChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { onChange, query, onRunQuery } = this.props;
+    onChange({ ...query, zoneNames: event.target.value });
+    console.log('zoneNames: ' + event.target.value);
+    // executes the query
+    onRunQuery();
+  };
+
+  onMetricNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { onChange, query, onRunQuery } = this.props;
+    onChange({ ...query, metricName: event.target.value });
+    console.log('metricName: ' + event.target.value);
+    // executes the query
+    onRunQuery();
+  };
+
+  render() {
+    const query = defaults(this.props.query, defaultQuery);
+    const { selectedReport, zoneNames, metricName } = query;
+
+    return (
+      <div className="gf-form">
+        <div>
+          <InlineField label="Report">
+            <AsyncSelect
+              loadOptions={this.getReports}
+              defaultOptions
+              value={selectedReport}
+              placeholder="Select a report"
+              onChange={this.onSelectReportsChange}
+            />
+          </InlineField>
+          <FormField
+            value={zoneNames || ''}
+            labelWidth={4}
+            inputWidth={24}
+            onChange={this.onZoneNamesChange}
+            label="Zones"
+            tooltip="Comma-separted zone names. Metrics for listed zones are added together."
+          />
+          <FormField
+            value={metricName || ''}
+            labelWidth={8}
+            inputWidth={20}
+            onChange={this.onMetricNameChange}
+            label="MetricName"
+            tooltip="Graphed metric's name. If empty, a name is generated."
+          />
+        </div>
+      </div>
+    );
+  }
+}
